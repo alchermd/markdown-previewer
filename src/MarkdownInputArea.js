7025baf2ab51b5d3c2722bf8a93ce4472f0173ca
alchermd <johnalcherdoloiras@gmail.com>
2017-07-28 01:59:50 +0800

    Add component state and handleChange method to handle both internal and parent state changes.

--- src/MarkdownInputArea.js
+++ src/MarkdownInputArea.js
@@ -2,9 +2,27 @@ import React from 'react'
 import './MarkdownInputArea.css'
 
 class MarkdownInputArea extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      text: this.props.inputText
+    }
+
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  /**
+   * 
+   * @param {object} event - an event object. 
+   */
+  handleChange(event) {
+    this.props.handleChange(event.target.value)
+    this.setState({text: event.target.value})
+  }
+
   render() {
     return (
-      <textarea className='MarkdownInputArea'>
+      <textarea className='MarkdownInputArea' value={this.state.text} onChange={this.handleChange}>
         {this.props.inputText}
       </textarea>
     )
