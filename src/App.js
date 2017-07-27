2b9263f4edd14ac7be32f814a36c8bdcdf39f778
alchermd <johnalcherdoloiras@gmail.com>
2017-07-28 01:48:49 +0800

    Save default text as a component state.

--- src/App.js
+++ src/App.js
@@ -4,15 +4,24 @@ import MarkdownOutputDisplay from './MarkdownOutputDisplay.js'
 import defaultText from './defaultText'
 
 class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      markdownText: defaultText
+    }
+  }
+
   render() {
+    const {markdownText} = this.state
+
     return (
       <div className="container">
         <div className="row">
           <div className="col-md-6">
-            <MarkdownInputArea inputText={defaultText}/>
+            <MarkdownInputArea inputText={markdownText}/>
           </div>
           <div className="col-md-6">
-            <MarkdownOutputDisplay outputText={defaultText} />
+            <MarkdownOutputDisplay outputText={markdownText} />
           </div>
         </div>
       </div>
