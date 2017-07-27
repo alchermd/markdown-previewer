2c20988482a19f93877109b885bd0ad0e829f472
alchermd <johnalcherdoloiras@gmail.com>
2017-07-28 01:44:58 +0800

    Moved defaultText data up to the App component.

--- src/MarkdownInputArea.js
+++ src/MarkdownInputArea.js
@@ -1,12 +1,11 @@
 import React from 'react'
 import './MarkdownInputArea.css'
-import defaultText from './defaultText'
 
 class MarkdownInputArea extends React.Component {
   render() {
     return (
       <textarea className='MarkdownInputArea'>
-        {defaultText}
+        {this.props.inputText}
       </textarea>
     )
   }
